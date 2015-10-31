c985ab9b387842a41058f684c091a4f6f7cebad2
roman-yagodin <roman.yagodin@gmail.com>
2015-10-31 10:16:51 +0300

    Generate markup for several folders at once

--- obrnadzor/eduprogram-table.cs
+++ obrnadzor/eduprogram-table.cs
@@ -19,62 +19,84 @@ public class Script
 	public void Run (string workingDir)
 	{
 		Directory.SetCurrentDirectory (NauHelper.CurrentDirectory);
+
 		var log = new Log ("eduprogram-table");
         string [] tags = {"oop", "ucheb_plan", "annot", "graf", "metod", "chislen", "perevod" };
+        var outFile = "output.html";
+        var baseDir = "/portals/0/eduprograms/{folder}/";
+        var counter = 0;
+
+        FileStream fs = null;
+        StreamWriter sw = null;
 
 		try
 		{
-			var files = (NauHelper.IsNothingSelected)? Directory.GetFiles (Directory.GetCurrentDirectory ()) : NauHelper.SelectedFiles;
+            // create output file
+            fs = new FileStream (outFile, FileMode.Create);
+            sw = new StreamWriter (fs);
 
-            var baseDir = "/portals/0/eduprograms/{folder}/";
-            var template = File.ReadAllText (Path.Combine (workingDir, "template.html"));
+            var directories = (NauHelper.IsNothingSelected)? Directory.GetDirectories (Directory.GetCurrentDirectory ()) : NauHelper.SelectedFiles;
 
-            var oop_title = File.ReadAllText ("oop.txt");
-            var folder = Path.GetFileName (Directory.GetCurrentDirectory()).Split (' ') [0];
-            var code = oop_title.Split (' ') [0];
+            Array.Sort (directories);
 
-            var outFile = "output.html";
-            var praktika_count = 1;
+            foreach (var directory in directories)
+            {
+                var dirName = Path.GetFileName (directory);
+                if (dirName.StartsWith ("__") || dirName.StartsWith ("~")) {
+                    continue;
+                }
 
-			foreach (string file in files)
-			{
-				try
-				{
-					var ext = Path.GetExtension (file);
+                var template = File.ReadAllText (Path.Combine (workingDir, "template.html"));
 
-					if (ext == ".pdf")
-					{
-						foreach (var tag in tags)
-                        {
-    						if (StartsWith (Path.GetFileName(file), tag))
-    						{
-                                template = template.Replace ("{" + tag + "}", Path.Combine  (baseDir, Path.GetFileName(file)) );
-    						}
+                Directory.SetCurrentDirectory (directory);
+
+                var oop_title = File.ReadAllText ("oop.txt");
+                var folder = Path.GetFileName (Directory.GetCurrentDirectory()).Split (' ') [0];
+                var code = oop_title.Split (' ') [0];
+
+                var praktika_count = 1;
+
+                var files = Directory.GetFiles (Directory.GetCurrentDirectory ());
+
+    			foreach (string file in files)
+    			{
+    				try
+    				{
+    					var ext = Path.GetExtension (file);
+
+    					if ((ext == ".pdf" || ext == ".xls") && !Path.GetFileName (file).StartsWith ("__"))
+    					{
+    						foreach (var tag in tags)
+                            {
+        						if (StartsWith (Path.GetFileName(file), tag))
+        						{
+                                    template = template.Replace ("{" + tag + "}", Path.Combine  (baseDir, Path.GetFileName(file)) );
+        						}
+                            }
+
+                            if (StartsWith (Path.GetFileName(file), "rp_"))
+                            {
+                                template = template.Replace ("{rp_praktika}",
+                                "<a href=\"" + Path.Combine  (baseDir, Path.GetFileName(file)) + "\" itemprop=\"EduPr\">" + (praktika_count++) + "</a> {rp_praktika}");
+                            }
                         }
+    				}
+    				catch (Exception ex)
+    				{
+    					log.WriteLine ("Error: " + ex.Message);
+    				}
+    			}
 
-                        if (StartsWith (Path.GetFileName(file), "rp_"))
-                        {
-                            template = template.Replace ("{rp_praktika}",
-                            "<a href=\"" + Path.Combine  (baseDir, Path.GetFileName(file)) + "\" itemprop=\"EduPr\">" + (praktika_count++) + "</a> {rp_praktika}");
-                        }
-                    }
-				}
-				catch (Exception ex)
-				{
-					log.WriteLine ("Error: " + ex.Message);
-				}
-			}
+                template = template.Replace ("{code}", code);
+                template = template.Replace ("{folder}", folder);
+                template = template.Replace ("{oop_title}", oop_title);
+                template = template.Replace ("{#}", (++counter).ToString ());
 
-            template = template.Replace ("{code}", code);
-            template = template.Replace ("{folder}", folder);
-            template = template.Replace ("{oop_title}", oop_title);
+                // clean tags
+                template = template.Replace ("{rp_praktika}", string.Empty);
 
-            // clean tags
-            template = template.Replace ("{rp_praktika}", string.Empty);
-
-            File.WriteAllText (outFile, template);
-
-            Command.Run ("atom", outFile);
+                sw.Write (template);
+            }
 		}
 		catch (Exception ex)
 		{
@@ -82,6 +104,13 @@ public class Script
 		}
 		finally
 		{
+            if (sw != null) {
+                sw.Close ();
+            }
+
+            if (fs != null) {
+                fs.Close ();
+            }
 			log.Close();
 		}
 	}
