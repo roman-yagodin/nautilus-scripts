ae38cd7fead1b941f3f09d77fc0586e261d1950a
roman-yagodin <roman.yagodin@gmail.com>
2015-11-10 11:25:20 +0300

    Add pdftk variant of pdf-to-pages

---
+++ pdf-to/pdf-to-pages-pdftk.cs
@@ -0,0 +1,46 @@
+#!/usr/bin/csexec -r:R7.Scripting.dll
+
+using System;
+using System.IO;
+using R7.Scripting;
+
+public static class Program
+{
+    public static int Main (string [] args)
+    {
+        new Script ().Run ();
+        return 0;
+    }
+}
+
+public class Script
+{
+	public void Run ()
+	{
+		Directory.SetCurrentDirectory (NauHelper.CurrentDirectory);
+		var log = new Log ("pdf-to-pages-pdftk");
+
+		try
+		{
+			foreach (var fileName in FileHelper.GetFiles (FileSource.Nautilus))
+			{
+				try
+				{
+					Command.Run ("pdftk", string.Format ("\"{0}\" burst", fileName));
+				}
+				catch (Exception ex)
+				{
+					log.WriteLine ("Error: " + ex.Message);
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			log.WriteLine ("Error: " + ex.Message);
+		}
+		finally
+		{
+			log.Close();
+		}
+	}
+}
