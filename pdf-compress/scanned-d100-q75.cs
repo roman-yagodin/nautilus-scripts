6b68358e8776faee20a6636eb8fbb610700fd761
roman-yagodin <roman.yagodin@gmail.com>
2016-11-09 13:24:41 +0300

    Add scanned PDF compression script with lower dpi and quality

---
+++ pdf-compress/scanned-d100-q75.cs
@@ -0,0 +1,63 @@
+#!/usr/bin/csexec -r:R7.Scripting.dll
+
+using System;
+using System.IO;
+using R7.Scripting;
+
+public static class Program
+{
+    public static int Main (string [] args)
+    {
+        new CompressScannedPdfScript ().Run ();
+        return 0;
+    }
+}
+
+public class CompressScannedPdfScript
+{
+	public void Run ()
+    {
+		Directory.SetCurrentDirectory (NauHelper.CurrentDirectory);
+        Directory.CreateDirectory ("~backup");
+        var log = new Log ("scanned-85");
+
+		try {
+			foreach (var file in FileHelper.GetFiles (FileSource.Nautilus)) {
+                var ext = Path.GetExtension (file).ToLowerInvariant ();
+                if (ext == ".pdf" && !FileHelper.IsDirectory (file)) {
+                    var outFile = ".compressed_" +   Path.GetFileName (file);
+    				try {
+    					Command.Run ("convert",
+                            string.Format ("-density 100x100 -quality 75 -compress jpeg \"{0}\" \"{1}\"",
+                                file, outFile));
+
+        				if (new FileInfo (file).Length > new FileInfo (outFile).Length) {
+        					// compression succeded, the compressed file size is less than original file size
+        					var backupFile = Path.Combine ("~backup", Path.GetFileName (file));
+        					
+                            // backup original file
+        					File.Copy (file, backupFile, true);
+        					File.Delete (file);
+
+                            // substitute original file
+        					File.Move (outFile, file);
+        				}
+        				else {
+        					// compression failed, size of the compressed file is greater or equal than original
+        					File.Delete (outFile);
+        				}
+    				}
+    				catch (Exception ex) {
+    					log.WriteLine ("Error: " + ex.Message);
+    				}
+                }
+			}
+		}
+		catch (Exception ex) {
+			log.WriteLine ("Error: " + ex.Message);
+		}
+		finally {
+			log.Close();
+		}
+	}
+}
