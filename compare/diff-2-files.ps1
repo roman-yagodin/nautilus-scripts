5b3bdb887c2e43dbdc8429152ed57fc1d3bed8cd
roman-yagodin <roman.yagodin@gmail.com>
2020-09-29 12:20:14 +0400

    Check for number of files

--- compare/diff-2-files.ps1
+++ compare/diff-2-files.ps1
@@ -7,12 +7,16 @@ Import-Module -Name Files
 Import-Module -Name Pwshx
 
 $selectedFiles = Get-NautilusSelectedFiles
+if (($selectedFiles | Measure-Object).Count -lt 2) {
+    Write-Error -Message "You must select at least 2 files to compare!"
+    Wait-IfTerminal
+    exit 1
+}
+
 $file1 = $selectedFiles | Select-Object -Index 0
 $file2 = $selectedFiles | Select-Object -Index 1
 
 diff @("--color", "-w", "-U", "3", """$file1""", """$file2""")
 
 Wait-IfTerminal
-
-
 
